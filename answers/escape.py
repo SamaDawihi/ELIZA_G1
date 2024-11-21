e5de7519e85ea627b3184535c27eb59a7a0a53e4
SamaDawihi <samadawihi@gmail.com>
2024-11-21 22:32:13 +0300

    add escapes

---
+++ answers/escape.py
@@ -0,0 +1,10 @@
+last_escape_counter = 0
+last_escape = [
+    "If it's not about AlHilal, I'm not interested!",
+    "Why talk about anything else when AlHilal exists?",
+    "Stick to the topic—AlHilal is all that matters!",
+    "AlHilal is life! Let's focus on the real champions.",
+    "Sorry, I only speak the language of AlHilal. Next question?"
+    "Ask me about Alhilal only",
+    "Stick to the topic"
+]
