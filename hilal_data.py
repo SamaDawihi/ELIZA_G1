79bb2f18e874d0f370fd1a45fe6fd379756833a9
Salwa Alshamrani <80104061@mobily.com.sa>
2024-11-21 20:49:00 +0300

    Add Achivemnts

--- hilal_data.py
+++ hilal_data.py
@@ -1,20 +1,13 @@
 Hilal_info = {
     "name": "Al Hilal",
     "founded": 1957,
-    "stadium": "King Fahad International Stadium",
-    "manager": "Jorge Jesus",
-    "nickname": "The Blue Waves",
-    "city": "Riyadh",
-    "official_website": "https://www.alhilal.com",
-}
-
-achievement = {
-        "asia": "4 times",
-        "Saudi League": "18 times",
-        "king Cup": "9 times",
-        "crown Prince Cup": "13 times",
-        "Saudi Super Cup": "3 times"
-    }
+    "stadium": "King Fahad international Stadium",
+    "achievement": {
+            "asia" : "4 times",
+            "Saudi League": "18 times",
+            "king Cup": "9 times"
+            }
+        }
 
 players_info = [
     {"name": "Salem Al-Dawsari", "position": "Wing", "number": 29},
@@ -41,13 +34,16 @@ players_info = [
     {"name": "Mohammed Al-Rubaie", "position": "Goalkeeper", "number": 17},
 ]
 
-former_players_info = [
-    {"name": "Yasser Al-Qahtani", "position": "Forward", "number": 20},
-    {"name": "Sami Al-Jaber", "position": "Forward", "number": 9},
-    {"name": "Mohammed Al-Shalhoub", "position": "Midfielder", "number": 10},
-    {"name": "Radoi Mirel", "position": "Midfielder", "number": 8},
-    {"name": "Thiago Neves", "position": "Midfielder", "number": 7},
-    {"name": "Ahmed Al-Dokhi", "position": "Defender", "number": 2},
-    {"name": "Osama Hawsawi", "position": "Defender", "number": 33},
-    {"name": "Abdullah Al-Sudairy", "position": "Goalkeeper", "number": 1},
+
+Achievements = [
+   {"title": "Saudi Pro League", "count": 19},
+    {"title": "King Cup", "count": 10},
+    {"title": "Saudi Crown Prince Cup", "count": 13},
+    {"title": "AFC Champions League", "count": 5},
+    {"title": "FIFA Club World Cup Runners-Up", "year": 2019},
+    {"title": "Saudi Super Cup", "count": 1, "year": 2024},
+    {"title": "Unbeaten Season in Saudi Pro League", "season": "2023–24", "goals_scored": 101, "goals_conceded": 23},
+    {"title": "AFC Champions League Semi-Finals", "year": 2024},
 ]
+
+
