85aaa0c6905c252f999cd0d6b1520b72c2cf4a45
SarahAlenazi1 <sarah.meteb1@gmail.com>
2024-11-24 00:08:38 +0300

    update

--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
+# إعداد الصفحة
 st.set_page_config(
-    page_title="Blue Home Page",
-    page_icon="🌟",
-    layout="centered"
+    page_title="Alhilal Club",
+    page_icon="⚽",
+    layout="centered",
+    initial_sidebar_state="auto",
 )
 
-# CSS
+# CSS لتنسيق النصوص
 st.markdown(
     """
     <style>
@@ -15,33 +17,30 @@ st.markdown(
         color: white;
         font-family: 'Arial', sans-serif;
     }
-    .main {
-        text-align: center;
-        padding: 50px;
-    }
     .title {
-        font-size: 3em;
+        font-size: 1.4em;
         font-weight: bold;
         margin-bottom: 10px;
+        text-align: left;
     }
     .description {
-        font-size: 1.5em;
-        margin-bottom: 30px;
+        font-size: 1.2em;
+        margin-bottom: 20px;
+        text-align: left;
     }
     </style>
     """,
-    unsafe_allow_html=True
+    unsafe_allow_html=True,
 )
 
-col1, col2 = st.columns([1,2], gap= 'medium')
-
-st.markdown("<div class='main'>", unsafe_allow_html=True)
+col1, col2 = st.columns([1, 2], gap="medium")
 with col1:
-    st.image("pages\\abu_rakan.png")
+    st.image("pages\\abu_rakan2.png")
 with col2:
-    st.markdown("<div class='title'>Welcome to Alhilal Club 🌟</div>", unsafe_allow_html=True)
-    st.markdown("<div class='description'>I am Mohammed, chat with me to get to know Alhilal</div>", unsafe_allow_html=True)
-
-st.write("continue here")           
-        
-st.markdown("</div>", unsafe_allow_html=True)
+    st.markdown("<div class='title'>Chat with Al-Hilal Big Fan Abu Rakan ⚽🌟</div>", unsafe_allow_html=True)
+    st.markdown(
+        "<div class='description'>Hi there!, chat with me to get to know Al-Hilal! We are here to talk about everything related to this amazing club.</div>",
+        unsafe_allow_html=True,
+    )
+st.write("---")
+st.write("Explore Alhilal Club's players, achievements, other sports and much more!")
