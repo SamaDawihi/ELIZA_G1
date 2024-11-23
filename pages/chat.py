f577fc391700b89ac38cfa0845211c3601e30d37
SamaDawihi <90503910+SamaDawihi@users.noreply.github.com>
2024-11-23 18:41:05 +0300

    uncomment

--- pages/chat.py
+++ pages/chat.py
@@ -25,36 +25,36 @@ def analyze_question(question):
 
 
     # check if question is in arabic
-    #if is_it_in_arabic(question):
-        #return update_counter("arabic_escape")
+    if is_it_in_arabic(question):
+        return update_counter("arabic_escape")
 
     
     # Hi, How are You
-    #if is_it_about_welcoming(question) and is_it_about_general_health(question):
-        #return get_welcome(question) + ", " + get_general_health_questions()
+    if is_it_about_welcoming(question) and is_it_about_general_health(question):
+        return get_welcome(question) + ", " + get_general_health_questions()
 
     # Hi, Hello, ...
-    #if is_it_about_welcoming(question):
-        #return get_welcome(question)
+    if is_it_about_welcoming(question):
+        return get_welcome(question)
     
     # How are You
-    #if is_it_about_general_health(question):
-        #return get_general_health_questions()
+    if is_it_about_general_health(question):
+        return get_general_health_questions()
     
-    #if is_it_about_farewell(question):
-        #return 'Bye'
+    if is_it_about_farewell(question):
+        return 'Bye'
     
     # if just 2 words. ex: "I like"
-    #if len(question) < 3 :
-            #return "what?"
+    if len(question) < 3 :
+            return "what?"
     
     # Not a question, Does not have [what, when, ...]
-    #if question_is_about(question) == 'not a question':        
-        #return update_counter("not_question")
+    if question_is_about(question) == 'not a question':        
+        return update_counter("not_question")
     
     # If it include escape topics. ex [nassr, injuries]
-    #if is_it_about_escape(question):
-        #return get_escape_answers(question)
+    if is_it_about_escape(question):
+        return get_escape_answers(question)
 
     if question_is_about(question) == 'yesno':
         return get_yesno_answers()
@@ -63,25 +63,27 @@ def analyze_question(question):
         return get_other_matches_answers(question)
 
     # Check if the question is about players
-    #if is_it_about_players(question): 
-        #return get_player_questions(question)
+    if is_it_about_players(question): 
+        return get_player_questions(question)
     
 
     # Check if the question is about achievements
     if is_it_about_achievements(question):
-        return get_achievements_info(question)
+        answer = get_achievements_info(question)
+        if answer:
+            return answer
 
     # Check if the question is about general information
-    #if is_it_about_other_sports_answers(question):
-        #return get_other_sports_answers(question)
+    if is_it_about_other_sports_answers(question):
+        return get_other_sports_answers(question)
 
 
     # Check if the question satisfies the club info condition
-    #if is_it_about_club(question):
-        #return get_club_answers(question)
+    if is_it_about_club(question):
+        return get_club_answers(question)
     
     # Default response if no known conditions are met
-    #return update_counter("last_escape")
+    return update_counter("last_escape")
 
 
 
