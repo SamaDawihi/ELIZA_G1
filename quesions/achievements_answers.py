c1e7d2a894488db3c8537f12c4433c1fafb84de3
SalwaAlshamrani <salwaalshamrani1@gmail.com>
2024-11-24 16:14:10 +0300

    Update achievements_answers.py

--- quesions/achievements_answers.py
+++ quesions/achievements_answers.py
@@ -48,6 +48,10 @@ def get_achievements_info(question):
         elif "king" in question:
             return "We won the King cup last year and also in 2023👏"
         
+        elif "last" in question and ("cup" in question or "cups" in question):
+            return "The last cup we won was the Saudi Super Cup against Al Nassr, where we beat them 4-1👏💙"
+
+        
 
     # If none of the above, return a empty message
     return ''
